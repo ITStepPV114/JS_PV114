2d746a97ac3d1618b251a09f4177fa9e54c1d25c
TSShrol <shrolts@gmail.com>
2023-06-21 19:03:41 +0300

    add example Drag and Drop

--- 05_DOM/practice/script.js
+++ 05_DOM/practice/script.js
@@ -5,17 +5,15 @@
 цифри. Число змінюється лише при натисканні на кнопки.
 */
 let counter = countnumber.innerText;
-console.log(counter);
-btnPlus.onclick=()=>
-{
+// console.log(counter);
+btnPlus.onclick = () => {
    counter++;
    countnumber.innerText = counter;
 }
 
-btnMinus.onclick=()=>
-{
+btnMinus.onclick = () => {
    counter--;
-   if(counter<=0)counter=0;
+   if (counter <= 0) counter = 0;
    countnumber.innerText = counter;
 }
 
@@ -25,3 +23,50 @@ btnMinus.onclick=()=>
 додаються кольорові блоки на сторінку. Блок має видалятися зі
 сторінки по кліку на сам блок
 */
+
+/* Створіть HTML-сторінку з трекбаром.
+Надайте користувачеві можливість змінювати положення
+синього вказівника. 
+povzunok
+*/
+let povzunok = document.querySelector(".povzunok");
+let slider = document.querySelector(".slider");
+console.dir(slider);
+
+povzunok.onmousedown = function (event) {
+   event.preventDefault();
+   console.dir(event.clientX);
+   //  console.dir(povzunok);
+   //  console.dir(povzunok.getClientRects()[0]);
+   console.dir(povzunok.getBoundingClientRect());
+   let currentX = povzunok.getBoundingClientRect().left;
+   //  let size=povzunok.getBoundingClientRect().width;
+   let stepX = event.clientX - currentX;
+   console.log(stepX);
+   //  povzunok.onmousemove;
+   povzunok.addEventListener("mousemove", onMouseMove);
+   povzunok.addEventListener("mouseup", onMouseUp);
+
+   function onMouseMove(event) {
+      console.log(event.ClientX);
+      let newMoveLeft = event.clientX - stepX - slider.getBoundingClientRect().left;
+      // console.log(slider.getBoundingClientRect().left);
+      if (newMoveLeft < 0) newMoveLeft = 0;
+      // console.log(newMoveLeft);
+      let newMoveRight=slider.offsetWidth-povzunok.offsetWidth;
+      if(newMoveLeft>newMoveRight) newMoveLeft=newMoveRight;
+      povzunok.style.left = newMoveLeft + "px";
+
+   }
+
+   function onMouseUp(event) {
+      povzunok.removeEventListener("mousemove", onMouseMove);
+      povzunok.removeEventListener("mouseup", onMouseUp);    
+   }
+   
+}
+
+document.ondragstart=function(){
+   return false;
+}
+
